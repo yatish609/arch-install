194a7798b9b9cb456ad100550a4bfdedc1808c93
Yatish Bhardwaj <yatishbhardwaj1@gmail.com>
2021-01-11 07:10:47 +0000

    Added option to install KDE

--- btrfs.sh
+++ btrfs.sh
@@ -18,5 +18,6 @@ pacman -Syy
 pacman -S reflector os-prober
 pacstrap /mnt base linux linux-firmware vim nano sudo vi base-devel linux-headers
 genfstab -U /mnt >> /mnt/etc/fstab
-sudo cp script2.sh /mnt
+sudo cp gnome-install.sh /mnt
+sudo cp kde-install.sh /mnt
 
